907dc8ed8927192784cb9cb6a4516ffb06b71b8a
I-Jac <isac.jacobsson03@gmail.com>
2025-01-21 12:29:40 +0100

    1

--- Public/Pages/MGGACoin/JupSwap/JupSwap.js
+++ Public/Pages/MGGACoin/JupSwap/JupSwap.js
@@ -1,23 +1,22 @@
-(window.Jupiter) {
-    window.Jupiter.init({
-        displayMode: "integrated",
-        integratedTargetId: "integrated-terminal",
-        endpoint: "https://misty-thrilling-scion.solana-mainnet.quiknode.pro/cf8404eb59e4ff88ff2ef1904ea16e8de1de0135/",
-        defaultExplorer: "Solscan",
-        enableWalletPassthrough: true,
-        passthroughWalletContextState: {
-            publicKey,
-            connected: !!publicKey,
-            signTransaction,
-            signAllTransactions,
-        },
-        formProps: {
-            fixedOutputMint: true,
-            fixedInputMint: true,
-            swapMode: "ExactIn",
-            initialAmount: "1000000000",
-            initialInputMint: "So11111111111111111111111111111111111111112",
-            initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
-            initialSlippageBps: 5,
-        },
-    });
+window.Jupiter.init({
+    displayMode: "integrated",
+    integratedTargetId: "integrated-terminal",
+    endpoint: "https://misty-thrilling-scion.solana-mainnet.quiknode.pro/cf8404eb59e4ff88ff2ef1904ea16e8de1de0135/",
+    defaultExplorer: "Solscan",
+    enableWalletPassthrough: true,
+    passthroughWalletContextState: {
+        publicKey,
+        connected: !!publicKey,
+        signTransaction,
+        signAllTransactions,
+    },
+    formProps: {
+        fixedOutputMint: true,
+        fixedInputMint: true,
+        swapMode: "ExactIn",
+        initialAmount: "1000000000",
+        initialInputMint: "So11111111111111111111111111111111111111112",
+        initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
+        initialSlippageBps: 5,
+    },
+});
