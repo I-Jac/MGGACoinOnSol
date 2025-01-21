13f2561d16687912e58bcb09c0dc634888316b02
I-Jac <isac.jacobsson03@gmail.com>
2025-01-21 12:41:02 +0100

    1

--- Public/Pages/MGGACoin/JupSwap/JupSwap.js
+++ Public/Pages/MGGACoin/JupSwap/JupSwap.js
@@ -3,20 +3,13 @@ window.Jupiter.init({
     integratedTargetId: "integrated-terminal",
     endpoint: "https://misty-thrilling-scion.solana-mainnet.quiknode.pro/cf8404eb59e4ff88ff2ef1904ea16e8de1de0135/",
     defaultExplorer: "Solscan",
-    enableWalletPassthrough: true,
-    passthroughWalletContextState: {
-        publicKey,
-        connected: !!publicKey,
-        signTransaction,
-        signAllTransactions,
-    },
     formProps: {
-        fixedOutputMint: true,
-        fixedInputMint: true,
-        swapMode: "ExactIn",
-        initialAmount: "1000000000",
-        initialInputMint: "So11111111111111111111111111111111111111112",
-        initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
-        initialSlippageBps: 5,
+      fixedInputMint: true,
+      fixedOutputMint: true,
+      initialAmount: "100000000",
+      initialInputMint: "So11111111111111111111111111111111111111112",
+      initialOutputMint: "FoyZoKXj8LxH29rND5AH5QcqGcH7FqeD8kjmN9zmpump",
+      initialSlippageBps: 5,
     },
-});
+  });
+  
